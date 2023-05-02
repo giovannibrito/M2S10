577f3a0bf11245662856a6626e8bb5e9ac9a8654
giovannibrito <giovannileonibrito@bol.com.br>
2023-05-01 22:49:30 -0300

    Adiciona exercicio 2

--- src/main/java/com/example/demo/controllers/ProdutoController.java
+++ src/main/java/com/example/demo/controllers/ProdutoController.java
@@ -1,13 +1,26 @@
 package com.example.demo.controllers;
 
+import com.example.demo.models.Produto;
+import com.example.demo.repositories.ProdutoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class ProdutoController {
+    private final ProdutoRepository repository;
 
-    @GetMapping("/produto")
-    public String exibeProduto(){
-        return "produto";
+    public ProdutoController(ProdutoRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/produtos")
+    public ModelAndView exibeProdutos(){
+        ModelAndView mav = new ModelAndView("produtos");
+        List<Produto> produtos = repository.findAll();
+        mav.addObject("produtos", produtos);
+        return mav;
     }
 }
---
+++ src/main/java/com/example/demo/models/Produto.java
@@ -0,0 +1,19 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private String descricao;
+    private String data;
+    private Double preco;
+}
